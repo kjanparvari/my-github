edfe3385c11cfedb1c56fecd6ed3e6fbd44dfba5
kjanp <kjanparvari@gmail.com>
2021-04-30 00:40:43 +0430

    it's done i think

--- js/script.js
+++ js/script.js
@@ -1,40 +1,63 @@
+// By Kamyar Janparvari - 9626015
+
+
+// whole code is wrapped in an arrow function
 (() => {
+    // selecting elements and assigning them to variables (it's self explanatory)
     const submitButton = document.getElementById("submitButton");
-    // const form = document.getElementById("form")
     const usernameTextBox = document.getElementById("username");
     const avatar = document.getElementById("avatar");
     const nameLabel = document.getElementById("name");
     const blogLabel = document.getElementById("blog");
-    const favLanLebel = document.getElementById("favLan");
+    const favLanLabel = document.getElementById("favLan");
     const locationLabel = document.getElementById("location");
     const bio = document.getElementById("bio");
     const infoContainer = document.getElementById("infoContainer");
     const errorContainer = document.getElementById("errorContainer");
     const errorPara = document.getElementById("errorMsg");
 
+    // attach handler to submit button
+    // it reads username from text box and calls getInfo() function to put values in the above elements
     window.onload = () => {
         submitButton.addEventListener('click', async () => {
             const username = usernameTextBox.value;
-            await getInfo(username);
+            if (username.trim() !== "")
+                await getInfo(username.trim());
         });
     }
 
-
+    /**
+     * gets a string argument as username and gets him/ her info from github and represent them using showInfo()
+     *
+     * @param username
+     * @returns {Promise<void>}
+     */
     const getInfo = async (username) => {
-        const url = `https://api.github.com/users/${username}`;
-        // console.log(`url: ${url}`);
-        try {
-            const cached = window.localStorage.getItem(username);
-            if (cached === null) {
+        // checking if the information is cached in local storage
+        const cached = window.localStorage.getItem(username);
+        if (cached === null) {
+            try {
+                // making url to use fetch api
+                const url = `https://api.github.com/users/${username}`;
+                // sending request to the url to get the info
                 await fetch(url).then(async (response) => {
-                    // console.log(response.status)
-                    if (response.status >= 400) {
+                    // if there was not such user
+                    if (response.status === 404) {
+                        showError(`Can Not Find Such User "${username}"`)
+                    }
+                    // if some other client or server error happened
+                    else if (response.status >= 400) {
                         const data = await response.json();
                         showError(data["message"]);
-                    } else if (response.status === 200) {
+                    }
+                    // if request was successful
+                    else if (response.status === 200) {
+                        // getting response body in json format
                         const rawData = await response.json();
+                        // finding favorite language
                         const favLan = await getFavoriteLanguage(rawData["repos_url"]);
                         console.log(favLan)
+                        // making an object of useful information that we need
                         const info = {
                             avatar: rawData["avatar_url"],
                             name: rawData["name"],
@@ -43,29 +66,43 @@
                             bio: rawData["bio"],
                             favLan: favLan
                         }
-                        // window.localStorage.setItem(data["login"], JSON.stringify(info));
+                        // caching it in local storage
+                        window.localStorage.setItem(username, JSON.stringify(info));
                         console.log(info)
+                        // representing them by calling showInfo()
                         showInfo(info);
                     }
                 });
-            } else {
-                showInfo(JSON.parse(cached))
+            } catch (e) {
+                // the above scope is about getting data from github and showing them
+                // so if something goes wrong following message will be shown on screen
+                showError('An Error Happened While Establishing Connection')
             }
-
-        } catch (e) {
-            showError('An Connection Error Happened')
+        } else {
+            // if data was cached, we don't send request anymore.
+            console.log("found in local storage.")
+            showInfo(JSON.parse(cached))
         }
     }
-
+    /**
+     * gets repositories url and returns favorite language
+     * @param reposLink
+     * @returns {Promise<Response>}
+     */
     const getFavoriteLanguage = async (reposLink) => {
         try {
+            // sending request to get repositories array
             return await fetch(reposLink).then(async (response) => {
+                // if some client or server error happens
                 if (response.status >= 400)
                     return " - ";
+                // if request was successful
                 else if (response.status === 200) {
                     const repos = await response.json();
-                    if (repos.length === 0)
+                    // if there was no repositories
+                    if (repos === null || repos.length === 0)
                         return " - "
+                    // a function for comparing 2 repository in order to sort the repos array
                     const compareRepos = (a, b) => {
                         if (Date.parse(a["pushed_at"]) < Date.parse(b["pushed_at"]))
                             return 1;
@@ -73,10 +110,12 @@
                             return -1;
                         return 0;
                     }
+                    // sorting the array based on pushed time
                     repos.sort(compareRepos)
                     const languages = {}
                     let cnt = 0;
                     let i = 0;
+                    // checking top 5 repositories and counting used languages
                     while (cnt <= 5) {
                         const lan = repos[i].language;
                         if (lan !== null) {
@@ -85,7 +124,6 @@
                             } else {
                                 languages[lan] = 1;
                             }
-                            // console.log(lan);
                             cnt++;
                         }
                         i++;
@@ -93,27 +131,35 @@
                             break
                     }
                     console.log(languages)
+                    // finding the language which is used most
                     const favLan = Object.keys(languages).reduce((a, b) => languages[a] > languages[b] ? a : b);
                     console.log(`favLan: ${favLan}`)
                     return favLan;
                 }
             });
         } catch (e) {
-            showError("An Connection Error Happened'")
+            showError('An Error Happened While Establishing Connection')
         }
 
     }
 
+    /**
+     * gets the user info and represent them in appropriate elements
+     * @param data
+     */
     const showInfo = (data) => {
+        // making error section invisible
         infoContainer.classList.remove("disabled");
+        // making info section visible
         if (!errorContainer.classList.contains("disabled"))
             errorContainer.classList.add("disabled");
+        // putting data in elements
         nameLabel.innerHTML = data["name"];
         blogLabel.innerHTML = data["blog"];
         blogLabel.href = data["blog"];
         locationLabel.innerHTML = data["location"];
         avatar.src = data["avatar"];
-        favLanLebel.innerHTML = data["favLan"];
+        favLanLabel.innerHTML = data["favLan"];
         bio.innerHTML = "";
         if (data["bio"] !== null) {
             data["bio"].split("\r\n").map((line) => {
@@ -126,10 +172,17 @@
 
     }
 
+    /**
+     * shows the error message in the error section
+     * @param msg
+     */
     const showError = (msg) => {
+        // making info section invisible
         errorContainer.classList.remove("disabled");
+        // making error section visible
         if (!infoContainer.classList.contains("disabled"))
             infoContainer.classList.add("disabled");
+        // putting the error message in the error section
         errorPara.innerHTML = msg;
     }
 })()
